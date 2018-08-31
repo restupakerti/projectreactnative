f4671eae53ab9740ba602780735f623e4eddbbdb
restupakerti <pakertirestu@gmail.com>
2018-08-31 10:34:07 +0700

    second commit

--- rnrestu/App.js
+++ rnrestu/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+
+
         <Text>WELCOME HOREYYY!</Text>
         <Text>Nama : RESTU PAKERTI</Text>
         <Text>Kelas : XI RPL 1</Text>
         <Text>No.Absen : 28 </Text>
+        <Image style={{ width: 400, height: 299}}
+        source={require('./saya.jpg')}
+         />
+       
       </View>
     );
   }
